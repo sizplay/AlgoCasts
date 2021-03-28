5e94964f9ef9626185bbbb22a5edfbdbc916b7b4
Chaehoon Lim <sizplay@gmail.com>
2021-03-28 21:42:29 +0900

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,27 +7,33 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// my answer
+// function vowels(str) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+
+//   const vowelCount = str
+//     .toLowerCase()
+//     .split("")
+//     .reduce((acc, cur) => {
+//       if (
+//         cur === "a" ||
+//         cur === "e" ||
+//         cur === "i" ||
+//         cur === "o" ||
+//         cur === "u"
+//       ) {
+//         acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
+//       }
+//       return acc;
+//     }, {});
+
+//   const total = vowels.reduce((acc, cur) => (acc += vowelCount[cur]), 0);
+//   return total || 0;
+// }
+
 function vowels(str) {
-  const vowels = ["a", "e", "i", "o", "u"];
-
-  const vowelCount = str
-    .toLowerCase()
-    .split("")
-    .reduce((acc, cur) => {
-      if (
-        cur === "a" ||
-        cur === "e" ||
-        cur === "i" ||
-        cur === "o" ||
-        cur === "u"
-      ) {
-        acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
-      }
-      return acc;
-    }, {});
-
-  const total = vowels.reduce((acc, cur) => (acc += vowelCount[cur]), 0);
-  return total || 0;
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
