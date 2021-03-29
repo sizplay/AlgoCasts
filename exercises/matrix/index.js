76ac1e124e7a49f1d0ec3b4eaac0c9c461097d6a
Chaehoon Lim <sizplay@gmail.com>
2021-03-29 21:56:36 +0900

    add matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,50 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(N) {
+  const array = [];
+
+  for (i = 0; i < N; i++) {
+    array.push([]);
+  }
+
+  let counter = 1;
+  let startRow = 0;
+  let endRow = N - 1;
+  let startCol = 0;
+  let endCol = N - 1;
+
+  while (startRow <= endRow && startCol <= endCol) {
+    // top row
+    for (let i = startCol; i <= endCol; i++) {
+      array[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // end col
+    for (let i = startRow; i <= endRow; i++) {
+      array[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+
+    // bottom row
+    for (let i = endCol; i >= startCol; i--) {
+      array[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // top col
+    for (let i = endRow; i >= startRow; i--) {
+      array[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+  }
+
+  return array;
+}
 
 module.exports = matrix;
